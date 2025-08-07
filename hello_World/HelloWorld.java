9426aafdc2b52bbe653aebc8d059c7f4a9c99d57
Desigveloper <devhabib60@gmail.com>
2025-08-07 18:05:06 +0000

    Update hello world

--- hello_World/HelloWorld.java
+++ hello_World/HelloWorld.java
@@ -5,10 +5,14 @@ import java.util.Scanner;
 public class HelloWorld {
 
     private static void sayGreetings(Scanner sc) {
+        String[] greetings = {"Hello", "Hi", "Hey", "Greetings", "Salutations"};
+        int randomIndex = (int) (Math.random() * greetings.length);
+
+
         System.out.print("What is you name? ");
         String name = sc.nextLine();
 
-        System.out.println("Hello, " + name + ", nice to meet you!");
+        System.out.println(greetings[randomIndex]  + ", " + name + ", nice to meet you!");
     }
     public static void main(String[] args) {
         sayGreetings(new Scanner(System.in));
