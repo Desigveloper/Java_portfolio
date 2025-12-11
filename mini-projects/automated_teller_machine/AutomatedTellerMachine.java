e786dccebd7e4493c071e269310204eae75a711b
Desigveloper <devhabib60@gmail.com>
2025-12-11 15:05:46 +0000

    Completed ATM

--- mini-projects/automated_teller_machine/AutomatedTellerMachine.java
+++ mini-projects/automated_teller_machine/AutomatedTellerMachine.java
@@ -5,25 +5,62 @@ import java.util.Scanner;
 public class AutomatedTellerMachine {  
     private static final Scanner sc = new Scanner(System.in);
 
-    private static boolean verifyUser(String password) {
+    private static boolean verifyUser(String pass) {
         boolean isVerified = false;
+        int attempts = 2;
+        String password = pass;
         
-        if (password.equals("12345")) 
-           isVerified = true;
+        do {
+            if (password.equals("12345")) {
+                isVerified = true;
+                break;
+            }
+           
+            
+            if (attempts == 0) {
+                System.out.println("PIN blocked!");
+                System.exit(0);
+            } else {
+                System.out.print("Wrong PIN! You have " + attempts  + " attempts left. Try again: ");
+                attempts--;
+                password = sc.nextLine();
+            }
+
+        } while(!isVerified);
+        
 
         return isVerified;
+        
     }
 
 
     public static void makeWithdrawal(int amount) {
+        int fiveDollarNote, twoDollarNote, oneDollarNote;
+        int tenDollarNote = fiveDollarNote = twoDollarNote = oneDollarNote = 0;
+        if (amount / 10 >= 1) {
+                tenDollarNote = amount / 10;
+                amount %= 10;
+
+        }
+        if (amount / 5 >= 1) {
+                fiveDollarNote = amount / 5;
+                amount %= 5;
+        }
         
-       while (amount > 0) {
-            if
-       }
+        if (amount / 2 >= 0) {
+                twoDollarNote = amount / 2;
+                amount %= 2;
+        }
+        
+        oneDollarNote = amount;
+
+       System.out.printf("%d notes(s) of $10, %d note(s) of $5, %d notes(s) of $2, and %d note(s) of $1\n",
+            tenDollarNote, fiveDollarNote, twoDollarNote, oneDollarNote
+       );
     }
     
     public static void main(String[] args) {
-         System.out.print("Enter your PIN: ");
+        System.out.print("Enter your PIN: ");
         String pin = sc.nextLine();
 
         boolean success = verifyUser(pin);
@@ -33,9 +70,7 @@ public class AutomatedTellerMachine {
             int amount = Integer.parseInt(sc.nextLine());
             makeWithdrawal(amount);
 
-            System.out.println("Money successfully withdrawn!");
-        } else {
-            System.out.println("You entered a wrong PIN");
+            System.out.println("Transaction successfully completed!");
         }
     }
 }
